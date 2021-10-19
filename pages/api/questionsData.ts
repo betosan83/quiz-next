e19f6278a63256ecbc52c6278d9fd8ca36b87a32
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-19 07:37:20 -0300

    New questions.

--- pages/api/questionsData.ts
+++ pages/api/questionsData.ts
@@ -38,6 +38,79 @@ const questions: QuestionModel[] = [
         AnswerModel.wrong('En Île de France '),
         AnswerModel.right('En Normandie'),
     ]),
+    new QuestionModel(7, 'Que signifie :', '"Être à la bourre"', [
+        AnswerModel.wrong('Être en avance'),
+        AnswerModel.wrong('Être à l’heure'),
+        AnswerModel.wrong('Être saoul'),
+        AnswerModel.right('Être en retard'),
+    ]),
+    new QuestionModel(8, 'Que signifie :', '“Être radin”', [
+        AnswerModel.wrong('Être généreux'),
+        AnswerModel.wrong('Être chanceux'),
+        AnswerModel.wrong('Être méchant'),
+        AnswerModel.right('Être avare'),
+    ]),
+    new QuestionModel(9, 'Quelle expression n’a pas le même sens que', '“Avoir la banane”', [
+        AnswerModel.wrong('Avoir la patate'),
+        AnswerModel.wrong('Avoir la frite'),
+        AnswerModel.wrong('Avoir la pêche'),
+        AnswerModel.right('Avoir la main verte'),
+    ]),
+    new QuestionModel(10, 'Que signifie l’expression :', '“Il n’y a pas un chat”', [
+        AnswerModel.wrong('Il n’y a rien à manger.'),
+        AnswerModel.wrong('Il n’y a rien à faire.'),
+        AnswerModel.wrong(' Il n’y a rien à voir.'),
+        AnswerModel.right('Il n’y a personne.'),
+    ]),
+    new QuestionModel(11, 'Quelle expression n’a pas le même sens que', '“J’en ai marre”', [
+        AnswerModel.wrong('J’en ai ras-le-bol'),
+        AnswerModel.wrong('J’en ai ma claque'),
+        AnswerModel.wrong('J’en ai assez'),
+        AnswerModel.right('J’en ai trop'),
+    ]),
+    new QuestionModel(12, 'Quelle est la bonne réponse ?', '“Il faut que je … le gâteau au four. ”', [
+        AnswerModel.wrong('mets'),
+        AnswerModel.wrong('met'),
+        AnswerModel.wrong('mettes'),
+        AnswerModel.right('mette'),
+    ]),
+    new QuestionModel(13, 'Quelle est la bonne réponse ?', '“J’espère que tu … à l’heure demain”', [
+        AnswerModel.wrong('soit'),
+        AnswerModel.wrong('es'),
+        AnswerModel.wrong('sois'),
+        AnswerModel.right('seras'),
+    ]),
+    new QuestionModel(14, 'Quelle est la bonne réponse ?', '“Je suis contente que tu … à la fac.”', [
+        AnswerModel.wrong('va'),
+        AnswerModel.wrong('iras'),
+        AnswerModel.wrong('vas'),
+        AnswerModel.right('ailles'),
+    ]),
+    new QuestionModel(15, 'Quelle est la bonne réponse ?', '“Elle pense que vous … tort”', [
+        AnswerModel.wrong('ayez'),
+        AnswerModel.wrong('alliez'),
+        AnswerModel.wrong('allez'),
+        AnswerModel.right('avez'),
+    ]),
+    new QuestionModel(16, 'Quelle est la bonne réponse ?', '“Il est peu probable qu’il … chanter.”', [
+        AnswerModel.wrong('sait'),
+        AnswerModel.wrong('sera'),
+        AnswerModel.wrong('save'),
+        AnswerModel.right('sache'),
+    ]),
+    new QuestionModel(17, 'Quelle est la bonne réponse ?', '“Elle fait une sieste après que son fils … mangé.”', [
+        AnswerModel.wrong('aie'),
+        AnswerModel.wrong('ait'),
+        AnswerModel.wrong('est'),
+        AnswerModel.right('a'),
+    ]),
+    new QuestionModel(18, 'Quelle est la bonne réponse ?', '“Je me doute bien que tu … la réponse”', [
+        AnswerModel.wrong('connaisse'),
+        AnswerModel.wrong('connait'),
+        AnswerModel.wrong('connaisses'),
+        AnswerModel.right('connais'),
+    ]),
+  
 ]
 
 export default questions
